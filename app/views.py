41de979b609700243892b0ac61a2a51df2cd6bc7
Joel Pareja <joel@jmpdebian0.localdomain>
2016-12-05 17:02:19 +0800

    Added login function

--- app/views.py
+++ app/views.py
@@ -1,8 +1,35 @@
+# Sample application for testing flask
+# by Joel Pareja
+#
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, world!"
+    user = {'nickname': 'Joel'}
+    posts = [
+	{
+	    'author': {'nickname': 'John'},
+	    'body': 'Beautiful day in Cebu' 
+	},
+	{
+	    'author': {'nickname': 'Susan'},
+	    'body': 'The Avenger movie was so cool!'
+	}
+    ]
+    return render_template('index.html', 
+	    		   title='Home',
+			   user=user,
+			   posts = posts)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+	flash('Login requested for OpenID="%s", remember_me=%s' %
+		(form.openid.data, str(form.remember_me.data)))
+	return redirect('/index')
+    return render_template('login.html',title='Sign In',form=form,providers=app.config['OPENID_PROVIDERS'])
 
---
+++ config.py
@@ -0,0 +1,10 @@
+#!/bin/env python
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'I am who I am'
+
+OPENID_PROVIDERS = [
+	{'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+	{'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+	{'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+	{'name': 'Flickr', 'url':'http://www.flickr.com/<username>'},
+	{'name': 'MyOpenID', 'url':'https://www.myopenid.com'}]
